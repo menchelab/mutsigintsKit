95c2853db617b64e60155c634e8df0ea01abf464
annahyan <anna@menchelab.com>
2023-06-27 12:39:23 +0200

    signature.annotations file is added as a global variable in the package

---
+++ R/global.R
@@ -0,0 +1,5 @@
+# Global package variables
+
+sig.annotations.path = system.file("data/signature.annotations.tsv", "mutsigintsKit")
+
+signature.annotations = read.delim(here("data/raw", "signature_annotations.tsv"))
