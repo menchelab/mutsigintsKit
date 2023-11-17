0f2e1d029a9f1e51c664cd1a2f182133fa8ef244
annahyan <anna@menchelab.com>
2023-11-17 13:03:17 +0100

    added the option of filter.mat. Sets interaction-wise thresholds

--- R/get_tissue_dataset_networks.R
+++ R/get_tissue_dataset_networks.R
@@ -1,33 +1,40 @@
-#' Extracts the interaction metrics of a tissue 
+#' Extracts the interaction metrics of a tissue
 #' from a list of interaction networks.
 #' @param tissue Tissue to be extracted
 #' @param network.lists The input list
-#' @param filter.list A list of metric values to be filtered out. The list 
-#' element names have the same names as metric names in network.lists. 
-#' The values specify filtering threshold. Everything below the number is 
+#' @param filter.list A list of metric values to be filtered out. The list
+#' element names have the same names as metric names in network.lists.
+#' The values specify filtering threshold. Everything below the number is
 #' set to 0. Default: NULL
+#' @param filter.mat If provided, this is a list of matrices, where the name of
+#' the element is the metric name. The list elements are matrices of all
+#' signature combinations in that tissue, where matrix elements are a predefined
+#' quantile of the null model of given sig-sig interaction. If provided the
+#' matrix will add another layer of filtering, where the absolute metric values
+#' smaller than the provided value are set to 0.
 #' @return A list with interaction metric matrices. The list element names match
 #' those in the input network.lists.
 #' @export
 
 get_tissue_dataset_networks = function(tissue,
-                                       network.lists, 
-                                       filter.list = NULL) {
-    
-    out.list = lapply(network.lists, function(x) 
+                                       network.lists,
+                                       filter.list = NULL,
+                                       filter.mat = NULL) {
+
+    out.list = lapply(network.lists, function(x)
         x[[tissue]])
-    
+
     if (is.null(filter.list)) {
         return(out.list)
-    } 
-    
+    }
+
     filter.list.absent = setdiff(names(filter.list), names(out.list))
-    
+
     if (length( filter.list.absent > 0))  {
-        stop(paste("filter.llist has variable names not present:", 
+        stop(paste("filter.list has variable names not present in network.lists:",
                    paste(filter.list.absent, collapse = " ")) )
-    } 
-    
+    }
+
     for (var in names(filter.list)) {
         varlim = filter.list[[var]]
         mat = out.list[[var]]
@@ -35,5 +42,14 @@ get_tissue_dataset_networks = function(tissue,
         mat = mat[rowSums(mat) > 0, colSums(mat) > 0]
         out.list[[var]] = mat
     }
+    # For each matrix for metric thresholds, all the elements in the
+    # calculated matrix that in absolute values are greater than the threshold
+    # are set to 0
+    if (! is.null(filter.mat)) {
+      for (metric.name in names(filter.mat)) {
+        out.list[[metric.name]] [ abs(out.list[[metric.name]]) < filter.mat[[metric.name]] ] = 0
+      }
+    }
+
     return(out.list)
 }
