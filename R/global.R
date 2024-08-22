71c5d89acd780da606d830c23e06925e23e858c2
annahyan <anna@menchelab.com>
2024-08-22 14:49:07 +0200

    added PCAWG.sigs.subset.rda file to the package

--- R/global.R
+++ R/global.R
@@ -1,5 +1,6 @@
 # # # Global package variables
 # #
-# signature.annotations = read.delim("data/signature_annotations.tsv")
-#
-# usethis::use_data(signature.annotations, overwrite = T)
+signature.annotations = read.delim("data/signature_annotations.tsv")
+PCAWG.sigs.subset = read.delim("data/PCAWG_sigProfiler_sigs_subset.tsv")
+
+usethis::use_data(signature.annotations, PCAWG.sigs.subset, overwrite = T)
